56fd0891c85ce217cc54c79f34e32deca1a47c69
GreatestGod <greagestgod@gmail.com>
2018-09-23 18:53:40 +0500

    Решение 3 домашнего задания

--- src/ru/javawebinar/basejava/storage/SortedArrayStorage.java
+++ src/ru/javawebinar/basejava/storage/SortedArrayStorage.java
@@ -3,37 +3,53 @@ package ru.javawebinar.basejava.storage;
 import ru.javawebinar.basejava.model.Resume;
 
 import java.util.Arrays;
+import java.util.Collections;
 
 public class SortedArrayStorage extends AbstractArrayStorage{
-    @Override
-    public void clear() {
-
-    }
-
-    @Override
-    public void update(Resume r) {
-
-    }
 
     @Override
     public void save(Resume r) {
-
+        if (size == 0) {
+            storage[0] = r;
+            size++;
+        } else if (getIndex(r.getUuid()) >= 0) {
+            System.out.println("Resume " + r.getUuid() + " already exist");
+        } else if (size >= STORAGE_LIMIT) {
+            System.out.println("Storage overflow");
+        } else {
+            int indexToInsert = 0;
+            for (int i = 0; i < size; i++) {
+                if (r.compareTo(storage[i]) > 0) {
+                    indexToInsert = i + 1;
+                } else {
+                    break;
+                }
+            }
+            Resume[] tmpResumeArray = new Resume[size - indexToInsert];
+            System.arraycopy(storage, indexToInsert, tmpResumeArray, 0, size - indexToInsert);
+            storage[indexToInsert] = r;
+            System.arraycopy(tmpResumeArray, 0, storage, ++indexToInsert, tmpResumeArray.length);
+            size++;
+        }
     }
 
     @Override
     public void delete(String uuid) {
-
-    }
-
-    @Override
-    public Resume[] getAll() {
-        return new Resume[0];
+        int index = getIndex(uuid);
+        if (index < 0) {
+            System.out.println("Resume " + uuid + " not exist");
+        } else {
+            int nextIndex = index + 1;
+            System.arraycopy(storage, nextIndex, storage, index, size - index);
+            size--;
+        }
     }
 
     @Override
     protected int getIndex(String uuid) {
         Resume searchKey = new Resume();
         searchKey.setUuid(uuid);
+        System.out.println(Arrays.binarySearch(storage, 0, size, searchKey) + " Это вернул поиск");
         return Arrays.binarySearch(storage, 0, size, searchKey);
     }
 }
