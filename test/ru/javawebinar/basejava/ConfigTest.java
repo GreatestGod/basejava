7b838f18d342d5633d8af606c6abcbe8d46d5cbc
GreatestGod <greagestgod@gmail.com>
2018-10-10 21:14:35 +0500

    HW13 Начало выполнения задания

---
+++ test/ru/javawebinar/basejava/ConfigTest.java
@@ -0,0 +1,31 @@
+package ru.javawebinar.basejava;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.io.File;
+
+import static org.junit.Assert.*;
+
+public class ConfigTest {
+
+    @Test
+    public void getStorageDir() {
+        Assert.assertEquals(new File("/home/alexey/IdeaProjects/basejava/src/ru/javawebinar/basejava/storage"), Config.get().getStorageDir());
+    }
+
+    @Test
+    public void getDbUrl() {
+        Assert.assertEquals("jdbc:postgresql://localhost:5432/resumes", Config.get().getDbUrl());
+    }
+
+    @Test
+    public void getDbUser() {
+        Assert.assertEquals("postgres", Config.get().getDbUser());
+    }
+
+    @Test
+    public void getDbPassword() {
+        Assert.assertEquals("postgres", Config.get().getDbPassword());
+    }
+}
---
+++ test/ru/javawebinar/basejava/storage/SQLStorageTest.java
@@ -0,0 +1,11 @@
+package ru.javawebinar.basejava.storage;
+
+import ru.javawebinar.basejava.Config;
+import ru.javawebinar.basejava.storage.serializer.JsonStreamSerializer;
+
+public class SQLStorageTest extends AbstractStorageTest {
+
+    public SQLStorageTest() {
+        super(new SqlStorage(Config.get().getDbUrl(), Config.get().getDbUser(), Config.get().getDbPassword()));
+    }
+}
